6479188b6880a3007ec2260b3497b2525ccaa018
maximilian-ruesch <xammaximus.ruesch@gmail.com>
2023-01-12 19:20:01 +0100

    Add station search component

---
+++ web/client/src/components/station-search.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="station-search">
+        <soro-text-field
+            label="Search for station or halt by name:"
+            class="search-text-field"
+            @change="event => currentQuery = event"
+        />
+
+        <soro-button
+            label="Search"
+            class="search-button"
+            @click="searchName"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import SoroTextField from '@/components/common/soro-text-field.vue';
+import SoroButton from '@/components/common/soro-button.vue';
+</script>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'StationSearch',
+
+    data() {
+        return {
+            currentQuery: null
+        };
+    },
+
+    methods: {
+        searchName() {
+            if (!this.currentQuery) {
+                return;
+            }
+
+            console.log(`Will now try searching for ${this.currentQuery}`);
+        },
+    },
+});
+</script>
+
+<style scoped>
+.station-search {
+    display: flex;
+    height: 60px;
+}
+
+.search-text-field {
+    width: 80%;
+    height: 100%;
+}
+
+.search-button {
+    width: 20%;
+    height: 100%;
+}
+</style>
