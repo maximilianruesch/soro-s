86014ebe1be8f90cd3f37d3e1294e3af16c1a37e
maximilian-ruesch <xammaximus.ruesch@gmail.com>
2023-02-23 15:03:33 +0100

    Promote unused vars to error

--- web/client/.eslintrc.cjs
+++ web/client/.eslintrc.cjs
@@ -70,5 +70,6 @@ module.exports = {
         ],
         '@typescript-eslint/no-explicit-any': 0,
         '@typescript-eslint/ban-ts-comment': 0,
+        '@typescript-eslint/no-unused-vars': ['error'],
     }
 };
