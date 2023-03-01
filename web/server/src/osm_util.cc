400b12335a82f3d188787b5c457ae07a17ec9925
Julian Weidert <JulianWeidert@users.noreply.github.com>
2023-03-02 00:26:07 +0100

    Return type of search item

--- web/server/src/osm_util.cc
+++ web/server/src/osm_util.cc
@@ -3,7 +3,22 @@
 namespace soro::server {
 
 
-
+	std::string map_type(const osm_type type) {
+		switch (type) {
+            case osm_type::HALT: 
+				return "hlt"; 
+				break;
+            case osm_type::STATION: 
+				return "station"; 
+				break;
+            case osm_type::MAIN_SIGNAL: 
+				return "ms"; 
+				break;
+			default: 
+				return "undefined";
+				break;
+		}
+	}
 
 
 }  // namespace soro::server
