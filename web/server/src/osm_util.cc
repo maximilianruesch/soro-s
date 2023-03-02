d0a8aea194bc394ae31da83783d9808476f7897f
maximilian-ruesch <xammaximus.ruesch@gmail.com>
2023-03-02 19:04:32 +0100

    Correctly format osm_util

--- web/server/src/osm_util.cc
+++ web/server/src/osm_util.cc
@@ -1,24 +1,12 @@
 #include "soro/server/osm_util.h"
 
 namespace soro::server {
-
-
-	std::string map_type(const osm_type type) {
-		switch (type) {
-            case osm_type::HALT: 
-				return "hlt"; 
-				break;
-            case osm_type::STATION: 
-				return "station"; 
-				break;
-            case osm_type::MAIN_SIGNAL: 
-				return "ms"; 
-				break;
-			default: 
-				return "undefined";
-				break;
-		}
-	}
-
-
+    std::string map_type(const osm_type type) {
+        switch (type) {
+            case osm_type::HALT: return "hlt";
+            case osm_type::STATION: return "station";
+            case osm_type::MAIN_SIGNAL: return "ms";
+            default: return "undefined";
+        }
+    }
 }  // namespace soro::server
