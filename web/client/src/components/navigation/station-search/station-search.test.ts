d6bd48a482f0f52126f70f388cfe0295320331ab
maximilian-ruesch <xammaximus.ruesch@gmail.com>
2023-03-04 02:00:42 +0100

    implement extended link tests

--- web/client/src/components/navigation/station-search/station-search.test.ts
+++ web/client/src/components/navigation/station-search/station-search.test.ts
@@ -1,13 +1,12 @@
 import { shallowMountWithDefaults } from '@test-utils/shallow-mount-with-defaults';
 import StationSearch from './station-search.vue';
 import { InfrastructureNamespace } from '@/stores/infrastructure-store';
-import { Mock } from 'vitest';
 import { ExtendedVueWrapper } from '@test-utils/test-utils';
 
 describe('station-search', async () => {
     let stationSearch: ExtendedVueWrapper;
-    let searchPositionFromName: Mock;
-    let setCurrentSearchedMapPosition: Mock;
+    const searchPositionFromName = vi.fn();
+    const setCurrentSearchedMapPosition = vi.fn();
     const infrastructureState = {
         currentInfrastructure: '',
         currentSearchTerm: '',
@@ -15,25 +14,24 @@ describe('station-search', async () => {
         currentSearchedMapPositions: [],
     };
 
-    beforeEach(async () => {
-        searchPositionFromName = vi.fn();
-        setCurrentSearchedMapPosition = vi.fn();
-        stationSearch = await shallowMountWithDefaults(StationSearch, {
-            store: {
-                [InfrastructureNamespace]: {
-                    namespaced: true,
-                    state: infrastructureState,
-                    mutations: { setCurrentSearchedMapPosition },
-                    actions: { searchPositionFromName },
-                },
+    const defaults = {
+        store: {
+            [InfrastructureNamespace]: {
+                namespaced: true,
+                state: infrastructureState,
+                mutations: { setCurrentSearchedMapPosition },
+                actions: { searchPositionFromName },
             },
-        });
+        },
+    };
+
+    beforeEach(async () => {
+        vi.clearAllMocks();
+        stationSearch = await shallowMountWithDefaults(StationSearch, defaults);
     });
 
     describe('when the search button emits a click event', async () => {
         it('does not call \'searchPositionFromName\' if no query is entered', function () {
-            expect.assertions(1);
-
             stationSearch.vm.currentQuery = null;
 
             const searchButton = stationSearch.findComponent({ ref: 'search-button' });
@@ -41,5 +39,37 @@ describe('station-search', async () => {
 
             expect(searchPositionFromName).not.toHaveBeenCalled();
         });
+    });
+
+    describe('when setting \'showExtendedLink\' to true', function () {
+        beforeEach(async () => {
+            stationSearch = await shallowMountWithDefaults(StationSearch, {
+                ...defaults,
+                props: { showExtendedLink: true },
+            });
+        });
+
+        it('shows an extended link', async () => {
+            const showExtendedLink = stationSearch.find('.station-search-extended-link');
+            expect(showExtendedLink.exists()).toBe(true);
+        });
+
+        it('emits \'change-to-extended\' when clicking the extended link', async () => {
+            const showExtendedLink = stationSearch.find('.station-search-extended-link');
+
+            await showExtendedLink.find('a').trigger('click');
+
+            expect(stationSearch.emitted('change-to-extended')).toHaveLength(1);
+        });
+    });
+
+    it('does not show an extended link when setting \'showExtendedLink\' to false', async () => {
+        stationSearch = await shallowMountWithDefaults(StationSearch, {
+            ...defaults,
+            props: { showExtendedLink: false },
+        });
+
+        const showExtendedLink = stationSearch.find('.station-search-extended-link');
+        expect(showExtendedLink.exists()).toBe(false);
     });
 });
