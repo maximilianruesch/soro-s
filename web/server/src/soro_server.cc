a4b78c0c58e0c476d518d7c370b1a34f05381f27
Goldkehle <118312001+Goldkehle@users.noreply.github.com>
2023-03-07 12:29:00 +0100

    Reverted merging artifact
    (when will we ever get rid of it...)

--- web/server/src/soro_server.cc
+++ web/server/src/soro_server.cc
@@ -136,19 +136,8 @@ void serve_file(std::string const& decoded_url,
 
 bool serve_tile(server::serve_context& sc, std::string const& decoded_url,
                 auto const& req, auto& res) {
-
-
-    std::cmatch regmatch;
-    
-    tiles::regex_matcher::match_result_t match;
-
-    if (std::regex_match(decoded_url.c_str(), regmatch, std::regex(R"(\/(\d+)\/(\d+)\/(\d+).mvt)"))) {
-
-        match = utl::to_vec(regmatch, [](auto const& m) {
-            return std::string_view{ m.first, static_cast<size_t>(m.length()) };
-            });
-    }
-
+  static tiles::regex_matcher const matcher{R"(\/(\d+)\/(\d+)\/(\d+).mvt)"};
+  auto const match = matcher.match(decoded_url);
   if (!match) {
     return false;
   }
