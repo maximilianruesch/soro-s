fcaabd49201e60a02eb85b849be0d080598fa91a
Goldkehle <118312001+Goldkehle@users.noreply.github.com>
2023-03-15 11:00:47 +0100

    Fixed up all documentation

--- generate-osm/db-utils/map-halts.go
+++ generate-osm/db-utils/map-halts.go
@@ -6,7 +6,8 @@ import (
 	"github.com/pkg/errors"
 )
 
-// mapUnanchoredMainSignals processes all main signals for which no unique Node could be determined.
+// mapHalts processes all halts.
+// This includes freight as well as passenger train halts.
 func mapHalts(
 	osmData *OSMUtil.Osm,
 	anchors map[float64]([]*OSMUtil.Node),
@@ -40,8 +41,8 @@ func mapHalts(
 	return nil
 }
 
-// serachUnanchoredMainSignal searches for a Node, that best fits the Signal to be mapped.
-// This search is based on at least two anchored elements and their respective distance to the signal at hand.
+// searchHalt searches for a Node, that best fits the halt to be mapped.
+// This search is based on at least two anchored elements and their respective distance to the halt at hand.
 // If no ore only one anchor could be identified, or all anchors are otherwise insufficient, no mapping can be done.
 func searchHalt(
 	osmData *OSMUtil.Osm,
